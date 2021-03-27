95574229947f4aba6f9847801fe0febbb0d02252
mert yıldırım <mert_1907fb@outlook.com.tr>
2021-03-27 23:27:55 +0300

    Proje dosyası ekle.

---
+++ Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        //ürün ıd'si
+        public int Id { get; set; }
+        //ürünün katogori ıd'si
+        public int CatagoryId { get; set; }
+        //ürün ismi
+        public string ProductName { get; set; }
+        //ürünün birim fiyatı
+        public double UnitPrice { get; set; }
+        //ürünü stok adedi
+        public int UnitInStocks  { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CatagoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStocks = 3;
+
+            Product product2 = new Product { Id = 2, CatagoryId = 5, UnitInStocks=5,
+            ProductName="Kalem", UnitPrice=35};
+
+            //PascalCase   //camelCase
+            //case sensitivy
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+
+        }
+    }
+}
